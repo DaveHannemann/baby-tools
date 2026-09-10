63ce69e464beba7b66d79a1fa711cd63a2024c9d
Dave Hannemann <davidhannemanndev@gmail.com>
2026-09-10 13:01:09 +0200

    fix: formatting

--- src/products/models.py
+++ src/products/models.py
@@ -48,6 +48,7 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.name
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True, null=False, blank=False)
 
